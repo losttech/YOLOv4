88cb36bf6b33179c75e5a92c83f157cbce35a646
Victor Milovanov <lost@losttech.software>
2020-11-08 18:57:23 -0800

    fixed nan loss when trainin
    port of a61f81f9118df9cec4d53736648174f6fb113e5f

--- src/keras/Activations.cs
+++ src/keras/Activations.cs
@@ -2,19 +2,9 @@
     using LostTech.Gradient;
     using LostTech.Gradient.ManualWrappers;
     static class Activations {
-        public static Tensor Softplus(IGraphNodeBase input, double threshold = 20) {
-            Tensor softlog(IGraphNodeBase input) => tf.log1p(tf.exp(input));
-
-            return tf.@case(
-                new (object, object)[]{
-                    (tf.greater(input, threshold), tf.identity_fn),
-                    (tf.less(input, -threshold), tf.exp_fn),
-                },
-                @default: PythonFunctionContainer.Of<IGraphNodeBase, Tensor>(softlog));
-        }
-
         public static Tensor Mish(IGraphNodeBase input)
-            => input * tf.tanh(tf.log1p(tf.exp(input)));
+            // https://github.com/hunglc007/tensorflow-yolov4-tflite/commit/a61f81f9118df9cec4d53736648174f6fb113e5f#diff-69d62c22a92472901b83e55ac7c153317c649564d4ae9945dcaed27d37295867R41
+            => input * tf.tanh(tf.nn.softplus(input));
 
         public static PythonFunctionContainer Mish_fn { get; }
             = PythonFunctionContainer.Of<IGraphNodeBase, Tensor>(Mish);
